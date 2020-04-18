751311791266f97320e3b6a6f64b594653e04240
r-arai316 <ryoutarou.arai@icloud.com>
2020-04-18 22:03:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-starter-ewpild
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-starter-ewpild)
---
+++ components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hello from './Hello'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
